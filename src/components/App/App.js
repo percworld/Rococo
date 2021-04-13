b59a6c157ee3b9c3933b01f327d342b980e2fad7
percworld <percworld@gmail.com>
2021-04-12 20:10:19 -0600

    Add apiCalls and move functions into it

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect, useReducer } from 'react';
 import { shuffleItems } from '../../utilities.js';
 import Wall from '../Wall/Wall';
 import ArtDetails from '../ArtDetails/ArtDetails.js';
+import { getIdObject, getArtByIndex } from '../../apiCalls';
 
 function App() {
   const [wall, setWall] = useState([]);
@@ -13,47 +14,35 @@ function App() {
   // const [ favorites, setFavorites ] = useState([]);
   //const [ searchTerms, setSearchTerms ] = useState([]);
   const searchTerm = 'q=sunflower'; // search terms that we made to state
-  const artIdSearch = fetch(`https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&${searchTerm}`)
-    .then(response => response.json())
-    .then(match => match.objectIDs)
-    .catch(error => setError(error.message));
 
-  // const artIdSearch = async () => {
-  //   try {
-  //     const response = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&${searchTerm}`);
-  //     let newList = await response.json()
-  //     console.log(newList.objectIDs);
-  //   } catch (error) {
-  //     setError(error.message)
-  //   }
-  //   return newList
-  // }
-  const getIDs = async () => {
-    const idMatches = await artIdSearch;
-    setError('');
-    setIDs(idMatches);
+  const getIDs = async (searchTerm) => {
+    try {
+      const idMatches = await getIdObject(searchTerm);
+      setIDs(idMatches);
+      setError('');
+    } catch (error) {
+      setError(error.message)
+    }
   }
 
 
   const getSingleArtPiece = async (index) => {
     try {
-      const item = fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${index}`)
-      const response = await item;
-      const artPiece = await response.json();
-      setWall(wall => [...wall, artPiece]);
+      const item = await getArtByIndex(index)
+      setWall(wall => [...wall, item]);
+      setError('');
     } catch (error) {
       setError(error)
     }
   }
 
   useEffect(() => {
-    getIDs();
+    getIDs(searchTerm);
   }, [])
 
   useEffect(() => {
-    // getIDs();
-
-    ids.length && getSingleArtPiece(shuffleItems(ids)[0]);
+    const wallArt = shuffleItems(ids);
+    ids.length && getSingleArtPiece(wallArt[0]);
     ids.length && getSingleArtPiece(shuffleItems(ids)[1]);
     ids.length && getSingleArtPiece(shuffleItems(ids)[2]);
     ids.length && getSingleArtPiece(shuffleItems(ids)[3]);
