1ddf86e5a00f9d25dafb1f7767c9e8d3f97bd837
percworld <percworld@gmail.com>
2021-04-17 20:58:11 -0600

    Add Tests in Cypress

--- cypress/integration/roccoco.spec.js
+++ cypress/integration/roccoco.spec.js
@@ -1,13 +1,13 @@
 const url = 'https://collectionapi.metmuseum.org/public/collection/v1/';
 describe('Get Images By Tags', () => {
-    208013,
-        203013,
-        10464,
-        10482,
-        11130,
-        470328,
-        11417,
-        11145
+    // 208013,
+    //     203013,
+    //     10464,
+    //     10482,
+    //     11130,
+    //     470328,
+    //     11417,
+    //     11145
     beforeEach(() => {
         cy.intercept(`${url}search?hasImages=true&q=canvas&q=painting&q=oil&`, { fixture: 'artIDs.json' })
         cy.intercept(`${url}objects/208013`, { fixture: 'artData1.json' })
@@ -16,7 +16,17 @@ describe('Get Images By Tags', () => {
     })
 
     it('should have a header', () => {
-        cy.get('button').should()
+        cy.get('button[className='logo - container']')
+            .contains('Roccoco')
+    })
+
+    it('should show the user a display of art from the Met', () => {
+        cy.get('.salonTemplate').children().should('have.length', 8)
+    })
+
+    it('should have images for each sorted artwork', () => {
+        cy.get('.div8')
+        //first().should('have.attr', 'src')
     })
 
 })
